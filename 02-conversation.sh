684301da2b559407ee846d4e75c652e77c7806de
prem DevOps Engineer <premdevops@gmail.com>
2026-02-02 21:10:33 +0530

    conversation bwtween two friend of event

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -5,3 +5,4 @@ echo "Mani:: I am doing well, thanks for asking."
 echo "prem:: What are your plans for the weekend?"
 echo "Mani:: I am thinking of going thiruvanamalai for girivalam. How about you?"
 echo "prem:: That sounds great! I planing to go sri Lakshmi semantham, I will share the loaction please join"   
+echo
