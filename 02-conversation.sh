42af7f1f331fc309ecd2d3f234a30a966d26a947
prem DevOps Engineer <premdevops@gmail.com>
2026-02-02 19:55:31 +0530

    conversation built between two friends

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Mani:: hello, prem how are you?"
+echo "prem:: Hi mani, I am fine. How about you?"
+echo "Mani:: I am doing well, thanks for asking."
+echo "prem:: What are your plans for the weekend?"
+echo "Mani:: I am thinking of going thiruvanamalai for girivalam. How about you?"
+echo "prem:: That sounds great! I planing to go sri Lakshmi semantham, I will share the loaction please join"   
