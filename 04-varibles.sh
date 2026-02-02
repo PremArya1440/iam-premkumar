5ea68d0bc54735d706554ba40f2ac048c6a9105e
prem DevOps Engineer <premdevops@gmail.com>
2026-02-02 23:30:29 +0530

    4th varibles nee $ tho replace chestuna names nee

---
+++ 04-varibles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2              
+PERSON3=$3
+
+echo "$PERSON1:: hello, $PERSON2 how are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How about you?"
+echo "$PERSON1:: I am doing well, thanks for asking."
+echo "$PERSON2:: What are your plans for the weekend?"
+echo "$PERSON1:: I am thinking of going thiruvanamalai for girivalam. How about you?"
+echo "$PERSON2:: That sounds great! I planing to go $PERSON3, I will share the loaction please join"   
