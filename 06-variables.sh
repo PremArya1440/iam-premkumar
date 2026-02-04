15094782511d52e2c50fca4b750db338b3a703d9
prem DevOps Engineer <premdevops@gmail.com>
2026-02-04 23:42:19 +0530

    sril lakshmi time ki nidhralestundi hahaahaaa

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-TIMESTAMP=$(date)
+START_TIME=$(date +%s)
 echo "script executed at: $TIMESTAMP"
 
+sleep 10
+END_TIME=$(date)
+TOTAL_TIME=$(($END_TIME -$START_TIME))
+echo "script executed at: $TOTAL_TIME "
