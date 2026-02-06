59a5bdfc1875710daeb34b3a470dba818e0d21f3
prem DevOps Engineer <premdevops@gmail.com>
2026-02-06 23:49:44 +0530

    nginx installation packages

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,4 +5,12 @@ if [ $USER -ne 0 ]; then
     exit 1
     fi
     echo "Installing the packag nginx the system..."
-    dnf install nginx 
+    dnf install nginx -y
+
+    if [$? -ne 0 ]; then
+    echo "installing nginx package failed."
+    exit 1
+    
+    else 
+    echo "installing nginx package is successful."
+    fi  
