8f6967d23ec291ee104f63b8d3e9677a656ae579
prem DevOps Engineer <premdevops@gmail.com>
2026-02-08 00:03:46 +0530

    19th tap script

---
+++ 19-trap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+echo "Hello prem"
+echo "when you are planing to  swti the job into DevOps"
+echo "I am preparing for DevOps, I will start learning, with in four months I will be ready to switch the job into DevOps"
+echoo "I am learning"
