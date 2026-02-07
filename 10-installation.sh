9a6f3fe08f582ea15e09c667aac033d286bccf11
prem DevOps Engineer <premdevops@gmail.com>
2026-02-07 11:16:45 +0530

    mysql installation packages

--- 10-installation.sh
+++ 10-installation.sh
@@ -14,3 +14,23 @@ if [ $USER -ne 0 ]; then
     else 
     echo "installing nginx package is successful."
     fi  
+
+    dnf install nginx -y
+
+    if [$? -ne 0 ]; then
+    echo "installing MySQL ..failed."
+    exit 1
+    
+    else 
+    echo "installing MySQL is successful."
+    fi
+
+    dnf install nodejs -y
+
+    if [$? -ne 0 ]; then
+    echo "installing nodejs ..failed."
+    exit 1
+    
+    else 
+    echo "installing nodejs is successful."
+    fi
