439d8c435d3038f68a39ff2c46682ab4a5969adb
prem DevOps Engineer <premdevops@gmail.com>
2026-02-07 20:58:24 +0530

    practicing the colors changing text in 17th files

---
+++ 17-color_script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/validation"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m" 
+
+
+if [ $USERID -ne 0 ]; then
+    echo -e ase run this script with root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R $2 ... FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$G $2 ... SUCCESS $Y" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@  #sudo sh-> all the arguments passed to the script
+ do
+    dnf install $package &>> $LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not installed, installing now"
+
+    dnf install $package -y &>> $LOGS_FILE
+    VALIDATE $? "Installing $package"
+        else
+            echo -e "$package is already installed, skipping installation" $Y SKIPPING $N"
+            
+        fi
+done
+
